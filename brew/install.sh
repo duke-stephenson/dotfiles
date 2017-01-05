11550cd0170b08577c6d0efc92a5e25fd7bda766
John Walker <john@chaosdevs.com>
2017-01-04 18:34:23 -0700

    chore(app): quick

--- brew/install.sh
+++ brew/install.sh
@@ -21,4 +21,48 @@ then
 
 fi
 
+# see https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
+brew doctor
+brew update
+
+sudo chown $USER /usr/local
+
+# core
+brew install coreutils
+
+# key commands
+brew install binutils
+brew install diffutils
+brew install findutils --with-default-names
+brew install gawk
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+
+# OS X ships a GNU version, but too old
+brew install emacs
+brew install gpatch
+brew install m4
+brew install make
+brew install nano
+
+# Other commands (non-GNU)
+brew install file-formula
+brew install git
+brew install less
+brew install openssh
+brew install python
+brew install rsync
+brew install svn
+brew install unzip
+brew install vim --with-override-system-vi
+
 exit 0
